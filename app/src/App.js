0c847b86d26809efb064a8cf6feb618b7d09d319
unknown <tompsherman@gmail.com>
2020-12-19 20:16:27 -0500

    job routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import './App.css'
 import JobCard from './components/JobCard'
@@ -8,10 +9,13 @@ import JobList from './components/JobList'
 const App = () => {
   return (
     <div className="App">
-      <h1>Build your resume:</h1>
-      <JobForm />
-      <JobList />
-      
+      <h1>Build your resume:</h1> 
+      <Route exact path='/'>
+          <JobList />
+      </Route>  
+      <Route exact path='/add'>
+          <JobForm />
+      </Route>
     </div>
   );
 };
